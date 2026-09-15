af7e503ef635fb345e9b17e6f601d88a666f37c0
shing <shintakjet@gmail.com>
2026-09-15 14:47:50 +0200

    finished teacher room 1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # Date: September 2026
 # -----------------------------------------------------------------------------
 
-from rooms import enterCorridor, enterClassroom2015, enterNSCorridor, enterStairExit, enterLab2001, enterEnchantedLibrary
+from rooms import enterCorridor, enterClassroom2015, enterNSCorridor, enterStairExit, enterLab2001, enterEnchantedLibrary, enterteacherroom1
 
 print("****************************************************************************")
 print("*                      Welcome to the School Maze!                         *")
@@ -27,6 +27,14 @@ state = {
         "nscorridor": False,
         "stairexit": False,
         # "teachersroom4": False,
+        "teacherroom1": False,
+    },
+    # TODO: consider moving this to room_states
+    "looked_around": {
+        "classroom2015": False,
+        "projectroom3": False,
+        "lab2001": False,
+        "teacherroom1": False,
     },
     # dictionary for tracking room-specific states
     "room_states": {
@@ -39,35 +47,6 @@ state = {
             "clue3_solved": False,
             "spellbook_unlocked": False,
         },
-        "lab2001": {
-            "workbench_examined": False,
-            "notebook_examined": False,
-            "laser_broken": False,
-            "resistance_setting": 0,
-            "given_voltage": 230,
-            "key_shards_placed": {
-                "key shard 1": False,
-                "key shard 2": False,
-                "key shard 3": False,
-                "key shard 4": False,
-            },
-            "final_key_forged": False,
-        },
-    },
-    # TODO: consider moving this to room_states
-    "looked_around": {
-        "classroom2015": False,
-        "projectroom3": False,
-        "lab2001": False,
-    },
-    # TODO: consider moving this to room_states
-    "looked_around": {
-        "classroom2015": False,
-        "projectroom3": False,
-        "lab2001": False,
-    },
-    # dictionary for tracking room-specific states
-    "room_states": {
         "lab2001": {
             "workbench_examined": False,
             "notebook_examined": False,
@@ -115,6 +94,9 @@ while True:
 
     elif current == "enchanted_library":
         state["current_room"] = enterEnchantedLibrary(state)
+
+    elif current == "teacherroom1":
+        state["current_room"] = enterteacherroom1(state)
 
     else:
         print("Unknown room. Exiting game.")
--- rooms/__init__.py
+++ rooms/__init__.py
@@ -16,3 +16,7 @@ from .classroom2015 import enterClassroom2015
 from .lab2001 import enterLab2001
 from .utils import chooseNextRoom
 from .enachanted_library import enterEnchantedLibrary
+from .teacherroom1 import enterteacherroom1
+
+
+
