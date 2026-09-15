1161044f860254fcc6e0cc3db57c6f483f4aef67
rafal <rafal.nedzarek@gmail.com>
2026-09-15 20:33:04 +0200

    deduplicated room_states in main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ state = {
         "stairexit": False,
         # "teachersroom4": False,
     },
+    # dictionary for tracking room-specific states
     "room_states": {
         "stairexit": {
           "opened_chest": False,
@@ -38,15 +39,6 @@ state = {
             "clue3_solved": False,
             "spellbook_unlocked": False,
         },
-    },
-    # TODO: consider moving this to room_states
-    "looked_around": {
-        "classroom2015": False,
-        "projectroom3": False,
-        "lab2001": False,
-    },
-    # dictionary for tracking room-specific states
-    "room_states": {
         "lab2001": {
             "workbench_examined": False,
             "notebook_examined": False,
@@ -61,6 +53,12 @@ state = {
             },
             "final_key_forged": False,
         },
+    },
+    # TODO: consider moving this to room_states
+    "looked_around": {
+        "classroom2015": False,
+        "projectroom3": False,
+        "lab2001": False,
     },
     "inventory": []
 }
